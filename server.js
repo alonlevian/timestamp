878bbaf8711c839bdf36ba926d2a8f74ea321738
Alon Levian <levianalon@gmail.com>
2021-05-05 15:59:08 +0300

    Change

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(env.process.PORT, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
